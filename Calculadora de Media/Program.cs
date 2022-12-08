38fb0288d3f82e47f2e6c090586d822abf0fef09
FilippiMnz <imperilkill12@gmail.com>
2022-12-08 15:54:38 -0300

    Bankaidayo

---
+++ Calculadora de Media/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Media
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a Primeira nota");
+            int nota1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a segunda nota");
+            int nota2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira a terceira nota");
+            int nota3 = int.Parse(Console.ReadLine());
+
+            int Media = (nota1 + nota2 + nota3) / 3;
+            
+            Console.WriteLine("A media do Aluno é {0}", Media);
+
+            if (Media <= 7)
+            {
+                Console.WriteLine("Rodou parça, ta de recuperação");
+            }
+            else
+            {
+                Console.WriteLine( "Passou chefe");
+            }
+        
+        }
+    }
+}
